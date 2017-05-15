cb10d4ceb5ed055c3eb99d5349bdd78df154ceb5
Salvatore Cascio <sacascio@cisco.com>
2017-05-15 15:27:45 -0400

    structures

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
 
-def test(*args):
-	return [x for x in args]
+def test(**kwargs):
+	c = kwargs
+	#print c['sal']
+	#print c['tom']
+	for f in c:
+		if f == 'tom':
+			print c[f]	
 
 
 def main():
-	p = 55
-	q = 56
-	print test(p,q)	
+
+	# DICTIONARY DEF
+	sample = {'sal' : 0, 'tom' : 99}
+	test(**sample)
+
+	# LIST DEFINITION	
+	w = ["5,6,7,8"]
+	print type(w)
 
 
+	
 main()
